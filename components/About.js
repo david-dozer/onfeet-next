4bcec94fbf977e9fc4ba3e7e6a389deed016e142
david-dozer <dgmendoza1015@gmail.com>
2024-10-03 14:13:49 -0500

    adjusted about section styling

--- components/About.js
+++ components/About.js
@@ -7,8 +7,8 @@ const About = () => {
       <div className="container px-4 px-lg-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div className="col-lg-8">
-            <h2 className="text-white mb-4">About onFeet</h2>
-            <p className="text-white-90">
+            <h2 className="text-white-80 mb-4">About onFeet</h2>
+            <p className="text-white-80">
               onFeet is on a mission to simplify sneaker shopping by bringing the mirror to you,
               enabling you to try on your wants and needs, seeing how they look, onFeet.
             </p>
